d20adeb5c8446bdcc12d51261d97aadc9cf62c63
Shahriar Parvez Shamim <spshamim90@gmail.com>
2024-06-09 00:18:18 +0600

    First Commit
    Routing and Parameter Passing

---
+++ introAgain/introAgain/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace introAgain.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            return View();
+        }
+        
+        public ActionResult Details(string id)
+        {
+            ViewData["Name"] = id;
+            return View();
+        }
+
+        public ActionResult Other()
+        {
+            return View();
+        }
+    }
+}
---
+++ introAgain/introAgain/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="introAgain.MvcApplication" Language="C#" %>
