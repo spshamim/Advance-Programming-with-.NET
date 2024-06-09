f08643fba5c1e1ceebc29795cf5671e1fda3e56d
Shahriar Parvez Shamim <spshamim90@gmail.com>
2024-06-09 14:31:52 +0600

    Portfolio
    Access Property Through Class

---
+++ Portfolio_/WebApplication1/WebApplication1/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string Course { get; set; }
+    }
+}
